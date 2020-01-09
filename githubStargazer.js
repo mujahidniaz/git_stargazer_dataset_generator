9f9e300c35fdc378d2f6b27e06f9b4b1688a17f0
mujahidniaz <mujahid.niaz002@gmail.com>
2020-01-09 18:24:51 +0500

    Code comments and ReadMe file updates

--- githubStargazer.js
+++ githubStargazer.js
@@ -1,10 +1,9 @@
-const axios = require('axios');
-const sampleNum = 15;
-let waitTime=200;
+const axios = require('axios'); // Using axios library for API Calls.
+let waitTime=200;                 // Wait time between API Calls in Milli seconds
 
-const token = '4d5235a5cbc0c47688da990461694a04518a2bcc';
-let resArray=[];
-let catchArray=[];
+const token = '4d5235a5cbc0c47688da990461694a04518a2bcc'; // Authentication token.
+let resArray=[];  // successfull api calls array.
+let catchArray=[]; // Failed API Calls array
 
 
 // return [1,2, ..., n]
@@ -16,14 +15,13 @@ const range = n => Array.apply(null, {length: n}).map((_, i) => i + 1);
  * @param {String} token - github access token
  * @return {Array} history - eg: [{date: 2015-3-1,starNum: 12}, ...]
  */
+
+ // Function that takes repo name and Aunthentication token to make api calls
  async function getStarHistory(repo, token) {
   const axiosGit = axios.create({
     headers: {
-      Accept: 'application/vnd.github.v3.star+json',
+      Accept: 'application/vnd.github.v3.star+json',   /// According Git-Hub API Documentation header must have this Type to get timestamped data other wise it will only return users.
     },
-    // params: {
-    //   access_token: token
-    // },
   });
 
   /**
@@ -37,8 +35,7 @@ const range = n => Array.apply(null, {length: n}).map((_, i) => i + 1);
     
     const initUrl = `https://api.github.com/repos/${repo}/stargazers`+'?access_token='+ token +'&per_page=100';   // used to get star info
     const initRes =  await axiosGit.get(initUrl);
-    //console.log(initRes);
-   
+
     /** 
      * link Sample (no link when star < 30):
      * <https://api.github.com/repositories/40237624/stargazers?access_token=2e71ec1017dda2220ccba0f6922ecefd9ea44ac7&page=2>;
@@ -54,18 +51,18 @@ const range = n => Array.apply(null, {length: n}).map((_, i) => i + 1);
     const pageNum = link ? /next.*?&page=(\d*).*?last/.exec(link)[1] : 1; // total page number
     console.log(pageNum);
 
-    if(pageNum>100)
+    if(pageNum>100)    /// if total api calls are less than 100 then delay will be defaul which is 200 milli seconds but in case of more API calls than 100 delay must be minimum of 1 second =  1000 milli seconds.
     {
       waitTime=1000;
     }
     
     
-    const pageIndexes = Array.from({length: pageNum}, (v, k) => k+1); 
+    const pageIndexes = Array.from({length: pageNum}, (v, k) => k+1); // creating dymanic page index array  
     
     console.log('test'+ pageNum);
     
     
-   const sampleUrls = pageIndexes.map(pageIndex => `${initUrl1}?page=${pageIndex}`+ '&access_token='+ token +'&per_page=100');
+   const sampleUrls = pageIndexes.map(pageIndex => `${initUrl1}?page=${pageIndex}`+ '&access_token='+ token +'&per_page=100'); /// Sample URLs for getting stargazer info.
     console.log(sampleUrls);
     console.log("pageIndexes", pageIndexes);
     return { firstPage: initRes, sampleUrls, pageIndexes };
@@ -74,34 +71,34 @@ const range = n => Array.apply(null, {length: n}).map((_, i) => i + 1);
   const { sampleUrls, pageIndexes, firstPage } = await generateUrls(repo);
 
   
-let paginArray =  chunkArray(sampleUrls,150)
+//let paginArray =  chunkArray(sampleUrls,100)        /// dividing into chunks so that we can make more API calls with less delay and make longer delay between chunks. but it didnt work so leaving it commented
 
-    for (let i = 0; i < sampleUrls.length; i++) {
+    for (let i = 0; i < sampleUrls.length; i++) {  // iterate over the pageURls and get 100 user per url
       console.log(i);
       
-      axiosGit.get(sampleUrls[i])
+      axiosGit.get(sampleUrls[i])  // Get 100 users for each page using Get Request to API
           .then((response) => {
 
             
            // console.log(response.data[0]);
-            response.data.forEach(element => {
-              let currdata = {repo_name:repo,user:"",starred_at:""};
-             currdata.starred_at= element.starred_at;
+            response.data.forEach(element => {   /// Iterate over 100 Users details and extract required data\
+              let currdata = {repo_name:repo,user:"",starred_at:""};  // Create a new Json object
+             currdata.starred_at= element.starred_at;               // Assign Values
              currdata.user=element.user.login;
-             resArray.push(currdata);
+             resArray.push(currdata);                               // push to result array
             });
             
           }).catch(error => {
             console.log(error);
-            catchArray.push(sampleUrls[i]);
+            catchArray.push(sampleUrls[i]);                       // if the URL fails . push it to the catchArray.
           })
       
-      await timer(waitTime); // then the created Promise can be awaited
+      await timer(waitTime); // delayed between requests.     
     }
 
 
-    await timer(5000);
-    var flatArray = Array.prototype.concat(...resArray);
+    await timer(5000);        // Wait for all the promisses (API Calls to finish)
+    var flatArray = Array.prototype.concat(...resArray); // merge multiple arrays from multiple alls into one.
     console.log(catchArray);
 
 // for (let i = 0; i < flatArray.length; i++) {
@@ -121,11 +118,12 @@ let paginArray =  chunkArray(sampleUrls,150)
   
 }
 
-
+/// Timer function that puts delay between API Calls.
 function timer(ms) {
   return new Promise(res => setTimeout(res, ms));
  }
 
+ // Dividing API Calls into chunks.. - NOt being used currently
  function chunkArray(myArray, chunk_size){
   var index = 0;
   var arrayLength = myArray.length;
@@ -140,7 +138,7 @@ function timer(ms) {
   return tempArray;
 }
  
-
+// Exporting module so that other functions can call.
 module.exports = {
 
 getStarHistory:async function(reponame) {
@@ -151,7 +149,7 @@ getStarHistory:async function(reponame) {
       console.log(err);
     });
     var end = new Date() - start
-  console.info('Execution time: %dms', end)
+  console.info('Execution time: %dms', end) /// prints time taken by the function.
   return history;
   //console.log( history );
 }
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express') //npm install express
   , bodyParser = require('body-parser') // npm install body-parser
-  , fs = require('fs')
-  ,path = require("path")
-  , http = require('http')
-  ,stargazer = require('./githubStargazer');
-  const port = 9898;
-const server = express()
-server.use(bodyParser.json());
-server.use("/public", express.static(path.join(__dirname, 'public')));
+  ,path = require("path")// including path liberary
+  , http = require('http')// npm install http liberary
+  ,stargazer = require('./githubStargazer');// including stargazer module from project 
+  const port = 9898;// App port.
+const server = express()    // // Creating express.js App
+server.use(bodyParser.json()); // Setting up body persor type as JSON for API End point.
+
+server.use("/public", express.static(path.join(__dirname, 'public')));   /// Setting 'Public' named folder as public for static contents like images and libraries
 server.listen(port, () => {
   console.log(`Server listening at ${port}`);
 });
@@ -17,16 +17,14 @@ server.listen(port, () => {
 
 
 server.get("/", (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');  /// returning the index.html file for the base url
 });
 
-server.get("/gitstargazer/",async(req, res) => {
+server.get("/gitstargazer/",async(req, res) => {        /// API GET end point which calls Gitstargazer Module's GetStarHistory function and provides the url for repo.
   let url = req.query.url;
-  console.log(url);
+  //console.log(url);
   result =await stargazer.getStarHistory(url)
   .then(result => res.send(result) )
   .catch(err => res.send(err))
-   
-  //res.send("hello");
 });
  
